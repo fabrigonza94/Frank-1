59b21980fad1b09490f9066c0311b9dc71ec7a1c
fabrigonza94 <fabrigonza1994@gmail.com>
2024-10-10 20:43:19 -0300

    first commit

---
+++ frank.uy - formulario/assets/js/custom.js
@@ -0,0 +1,66 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+$(".tool-mensaje").tooltip();
+
+//year
+$(function(){var d = new Date();$('.year').html(d.getFullYear() + " Â©");}); 
+
+
+//show more
+$("#show-more").click(verDetalle);
+    function verDetalle () {
+    if ($(".info-hide").css("display")=="none") 
+    {
+        $(".info-hide").slideDown("slow");
+    } else {
+        $(".info-hide").slideUp("slow");
+    }
+    $(this).hide();
+}
+
+
+
+
+
+
+$('.carousel[data-type="multi"] .item').each(function() {
+	var next = $(this).next();
+	if (!next.length) {
+		next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+
+	for (var i = 0; i < 2; i++) {
+		next = next.next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+
+		next.children(':first-child').clone().appendTo($(this));
+    }
+});
---
+++ frank.uy - formulario/bat/MailHandler-subscriptions.php
@@ -0,0 +1,46 @@
+<?php
+	$owner_email = $_POST["owner_email"];
+	$headers = 'From:' . $_POST["email"];
+	$subject = 'Suscripcion de ' . $_POST["name"];
+	$messageBody = "";
+	
+	if($_POST['name']!='nope'){
+		$messageBody .= '<p>Nombre: ' . $_POST["name"] . '</p>' . "\n";
+		$messageBody .= '<br>' . "\n";
+	}
+	if($_POST['phone']!='nope'){		
+		$messageBody .= '<p>Telefono: ' . $_POST['phone'] . '</p>' . "\n";
+		$messageBody .= '<br>' . "\n";
+	}
+	if($_POST['email']!='nope'){
+		$messageBody .= '<p>E-mail: ' . $_POST['email'] . '</p>' . "\n";
+		$messageBody .= '<br>' . "\n";
+	}else{
+		$headers = '';
+	}
+	if($_POST['state']!='nope'){		
+		$messageBody .= '<p>State: ' . $_POST['state'] . '</p>' . "\n";
+		$messageBody .= '<br>' . "\n";
+	}	
+	if($_POST['fax']!='nope'){		
+		$messageBody .= '<p>Fax Number: ' . $_POST['fax'] . '</p>' . "\n";
+		$messageBody .= '<br>' . "\n";
+	}
+	if($_POST['message']!='nope'){
+		$messageBody .= '<p>Mensaje: ' . $_POST['message'] . '</p>' . "\n";
+	}
+	
+	if($_POST["stripHTML"] == 'true'){
+		$messageBody = strip_tags($messageBody);
+	}
+	
+	try{
+		if(!mail($owner_email, $subject, $messageBody, $headers)){
+			throw new Exception('mail failed');
+		}else{
+			echo 'mail sent';
+		}
+	}catch(Exception $e){
+		echo $e->getMessage() ."\n";
+	}
+?>
